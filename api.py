d16c21c9271f95d5a2169ff4305aeffd1f7c1aa1
lingehhhc <lingehhhc@163.com>
2024-10-08 16:58:47 +0800

    add paint

--- api.py
+++ api.py
@@ -6,9 +6,15 @@ def getboard():
     for i in range(1, 1800000, 3):
         x = (i // 3) % 600
         y = (i // 3) // 600
-        color = hex(ord(res[i]))[2:].rjust(2,"0")+hex(ord(res[i+1]))[2:].rjust(2,"0")+hex(ord(res[i+2]))[2:].rjust(2,"0")
+        color = hex(ord(res[i]))[2:].rjust(2,"0")\
+              + hex(ord(res[i+1]))[2:].rjust(2,"0")\
+              + hex(ord(res[i+2]))[2:].rjust(2,"0")
         board[x][y] = color
     return board
+
+def paint(mx, my, mcolor, muid, mtoken):
+    data = {'x': mx, 'y': my, 'color': mcolor, 'uid': muid,  'token': mtoken}
+    x = requests.post("https://pbd.uwuwu.us.kg/paintboard/paint", data=data)
 
 if __name__ == "__main__":
     getboard()
